850fe4551a24a1cb88616888f243418c02c3e039
Herberth Henrique <herberth.lopes@gmail.com>
2023-06-16 17:36:58 -0300

    imprimindo saída no terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -6,19 +6,25 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in);
         
         // Solicitando Dados do Cliente
-        System.out.println("Por favor digite o número da conta: ");
-        int numero = scanner.nextInt();
 
         System.out.println("Por favor digite o número da Agência: ");
         int agencia = scanner.nextInt();
 
-        System.out.println("Por favor digite o nome do cliente: ");
-        String nome = scanner.next();
+        System.out.println("Por favor digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor digite o seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor digite o seu sobrenome: ");
+        String sobrenomeCliente = scanner.next();
 
         System.out.println("Por favor digite o valor do saldo: ");
         double saldo = scanner.nextDouble();
 
-        
+        // imprimindo saída no terminal
+
+        System.out.println("Olá " + nomeCliente.concat(" ").concat(sobrenomeCliente) + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numero + " e seu saldo "+ saldo + " já está disponível para saque.");
         
     }
 }
