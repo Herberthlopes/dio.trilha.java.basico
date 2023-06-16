8ec2817f3d9079329265993ce859159b5d85b912
Herberth Henrique <herberth.lopes@gmail.com>
2023-06-16 18:55:34 -0300

    alterado valor da nota

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,7 +2,7 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-       int nota = 6;
+       int nota = 8;
        
        if(nota >= 7)
         System.out.println("Aprovado");
