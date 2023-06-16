1b26c221e0e8c18641a6c38d37b2a69145f07fe2
Herberth Henrique <herberth.lopes@gmail.com>
2023-06-16 17:14:16 -0300

    efetuado solicitação de entrada de dados

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        
+        // Solicitando Dados do Cliente
+        System.out.println("Por favor digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor digite o número da Agência: ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Por favor digite o nome do cliente: ");
+        String nome = scanner.next();
+
+        System.out.println("Por favor digite o valor do saldo: ");
+        double saldo = scanner.nextDouble();
+
+        
+        
     }
 }
