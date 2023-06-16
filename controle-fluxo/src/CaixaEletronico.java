406e62e5b10661473ede5cb7afc9dd231104472c
Herberth Henrique <herberth.lopes@gmail.com>
2023-06-16 18:50:08 -0300

    alterado valor solicitado

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,5 +1,14 @@
+// CaixaEletronico.java
 public class CaixaEletronico {
     public static void main(String[] args) {
-        
+
+       double saldo = 25.0;
+       double valorSolicitado = 26.0;
+
+       if(valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado;
+
+        System.out.println(saldo);
+
     }
 }
