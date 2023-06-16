20c2396246cc85ff7b255b38e4cb2efac34c9677
Herberth Henrique <herberth.lopes@gmail.com>
2023-06-16 19:06:12 -0300

    novo bloco condicional adicionado

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,12 +2,15 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-       int nota = 8;
-       
-       if(nota >= 7)
-        System.out.println("Aprovado");
+        int nota = 5;
 
-       else
-        System.out.println("Reprovado");
+        if (nota >= 7)
+            System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova de Recuperação");
+
+        else
+            System.out.println("Reprovado");
     }
 }
