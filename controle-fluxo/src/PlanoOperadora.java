26a3bc8a2e3baebfe89b513d6eec5d5d009d3c14
Herberth Henrique <herberth.lopes@gmail.com>
2023-06-16 19:26:55 -0300

    criado nova classe

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,19 @@
+// Modo condicional convencional
+// Modo condicional switch/case
+public class PlanoOperadora {
+	public static void main(String[] args) {
+		String plano = "T"; // M / T
+
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("WhatsApp e Instagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+		}
+	}
+}
